acd8b4a28839fc7cc46e86c408c7c201d8f31720
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-06-26 14:16:12 +0300

    3

--- lib/view/screen/home_page.dart
+++ lib/view/screen/home_page.dart
@@ -4,6 +4,8 @@ import 'package:sneakertute/view/components/bottom_nav_bar.dart';
 import 'package:sneakertute/view/screen/cart_page.dart';
 import 'package:sneakertute/view/screen/shop_page.dart';
 
+import '../../core/const/images.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -12,11 +14,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selecteIndex=0;
+  int _selectedIndex=0;
 
   void navigateBottomBar( int index){
     setState(() {
-      _selecteIndex=index;
+      _selectedIndex=index;
     });
   }
 
@@ -31,7 +33,46 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar:MyBottomNavBar(
         onTabChange:(index) => navigateBottomBar( index) ,
       ),
-      body:_pages[_selecteIndex],
+      appBar:AppBar(
+        backgroundColor:Colors.grey[300],
+        elevation: 0,
+        // leading:IconButton(onPressed:() {
+        //   Scaffold.of(context).openDrawer();
+        // } , icon: Icon(Icons.menu,color:Colors.black,)),
+      ),
+      drawer:Drawer(
+        backgroundColor: Colors.grey[900],
+        child:Column(children: [
+          Padding(padding: const EdgeInsets.all(10),child:Image.asset(AssetsImage.nike,
+          color:Colors.white,),),
+
+          Padding(padding: const EdgeInsets.symmetric(horizontal: 25),
+          child:Divider(color:Colors.grey[800],),),
+
+          const Padding(
+            padding: EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              leading:Icon(Icons.home,color:Colors.white),
+              title:Text("Home",style:TextStyle(color:Colors.white),),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              leading:Icon(Icons.info,color:Colors.white),
+              title:Text("About",style:TextStyle(color:Colors.white),),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              leading:Icon(Icons.logout,color:Colors.white),
+              title:Text("Logout",style:TextStyle(color:Colors.white),),
+            ),
+          )
+        ],),
+      ),
+      body:_pages[_selectedIndex],
     );
   }
 }
